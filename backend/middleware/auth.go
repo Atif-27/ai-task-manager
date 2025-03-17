d36510a12ad28c379fe141ea09ca529bd0bcff1c
Atif <atif276ali@gmail.com>
2025-03-17 20:40:30 +0530

    Add JWT authentication middleware

---
+++ backend/middleware/auth.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware(c *fiber.Ctx) error {
+	tokenStr := c.Get("Authorization")
+
+	if tokenStr == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Unauthorized"})
+	}
+
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+
+	if err != nil || !token.Valid {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid token"})
+	}
+
+	return c.Next()
+}
