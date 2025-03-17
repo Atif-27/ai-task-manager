74e9745cd25e3d98ec7a51447dfad480b97ddb00
Atif <atif276ali@gmail.com>
2025-03-17 19:00:39 +0530

    Add MongoDB connection and Docker Compose setup

---
+++ database/db.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Client
+
+func ConnectDB() {
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGO_URI"))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Could not connect to MongoDB")
+	}
+
+	log.Println("Connected to MongoDB")
+	DB = client
+}
+
+func GetCollection(collectionName string) *mongo.Collection {
+	return DB.Database(os.Getenv("DB_NAME")).Collection(collectionName)
+}
